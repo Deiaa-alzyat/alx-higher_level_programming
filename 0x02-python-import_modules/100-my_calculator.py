04042e6d17a272b3701ffc59353127e5988fec17
Deiaa-alzyat <yousef.alzyat11@gmail.com>
2023-10-05 07:01:47 -0700

    0x01. Python - if/else, loops, functions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    if len(argv) != 4:
+    import sys
+
+    n = len(sys.argv) - 1
+    if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        quit(1)
+        sys.exit(1)
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from sys import argv
-    add = 0
-    for s in argv[1:]:
-        add += int(s)
-        print("{}".format(add))
+    """Print the addition of all arguments."""
+    import sys
+
+    ad = 0
+    for x in sys.argv:
+        if x != sys.argv[0]:
+            ad += int(x)
+    print(ad)
