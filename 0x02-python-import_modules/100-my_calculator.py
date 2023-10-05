c1dae87254049f4a2491791297ce88aa3df43369
Deiaa-alzyat <yousef.alzyat11@gmail.com>
2023-10-05 07:38:39 -0700

    0x01. Python - if/else, loops, functions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,17 +2,14 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-
     n = len(sys.argv) - 1
     if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
     ar_op = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(ar_op.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     print("{} {} {} = {}".format(a, sys.argv[2], b, ar_op[sys.argv[2]](a, b)))
