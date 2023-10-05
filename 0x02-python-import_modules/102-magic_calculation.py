1f1c697ee775495ae45054c9ac01672c13e060bb
Deiaa-alzyat <yousef.alzyat11@gmail.com>
2023-10-05 07:46:45 -0700

    0x01. Python - if/else, loops, functions

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,5 +2,10 @@
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
+        f = add(a, b)
+        for x in range(4, 6):
+            f = add(f, x)
+        return (f)
+
+    else:
+        return(sub(a, b))
