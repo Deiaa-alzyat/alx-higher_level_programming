529cf315e3a055d708659509b4101bf41bca2392
Deiaa-alzyat <yousef.alzyat11@gmail.com>
2023-10-24 10:44:46 -0700

    0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a square """
+class Square:
+    """Represents a square"""
+    def __init__(self, size):
+        """To initialize the square class
+        size - is the size of the defined square
+        """
+        self.__size = size
