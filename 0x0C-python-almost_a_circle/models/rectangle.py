d2e0b1e3fe1d7a51dbbcde9757610c3f55e29bb5
Deiaa-alzyat <yousef.alzyat11@gmail.com>
2023-11-10 12:50:37 -0800

    0x0C. Python - Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Creating a rectangle class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """The class Rectangle that inherits from Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializing the class Rectangle"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """The width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setting the width of the rectangle"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """The height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setting the height of the rectangle"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """The x coordinate of the rectangle"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setting the x coordinate of the rectangle"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """The y coordinate of the rectangle"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setting the y coordinate of the rectangle"""
+        self.__y = value
+
