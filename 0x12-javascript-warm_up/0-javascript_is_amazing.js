69ca0ee337f7f23c24b0fa84857f3d89fba15b07
Deiaa-alzyat <yousef.alzyat11@gmail.com>
2024-01-08 06:27:21 -0800

    0x12. JavaScript - Warm up

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// a script that prints “JavaScript is amazing” by using 'node' intepreter
+
+// To create const variable called myVar and assign string to it.
+const myVar = 'JavaScript is amazing';
+
+// console.log() function is used to print the value of myVar to the console.
+console.log(myVar);
