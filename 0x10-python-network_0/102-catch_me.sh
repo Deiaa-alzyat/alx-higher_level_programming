38bb86436834610239d9464de239174198770128
Deiaa-alzyat <yousef.alzyat11@gmail.com>
2024-01-25 07:00:41 -0800

    0x10. Python - Network #0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Script that makes a request to causes an specific response
-curl -sL -X PUT -H "You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
+
+# Make a request to 0.0.0.0:5000/catch_me that causes the server to respond with "You got me!"
+curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" -o /dev/null -w "You got me!"
